d8ce592cdd8eacd1a991fd1df496a4e35b2411b3
vitaliytv <v@nitra.ai>
2024-04-03 21:12:07 +0300

    abinbevefes/ft#442

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     "stylelint-config-standard-scss", // configure for SCSS
     "stylelint-config-recommended-vue", // add overrides for .Vue files
-    "stylelint-config-prettier", // turn off any rules that conflict with Prettier
   ],
   plugins: [],
   rules: {
